d7e8ff9d6d0abbf15dd850ffc8bc7026d0d98410
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-04-24 01:13:50 +0500

    Connect MySQL Database

---
+++ src/middlewares/db.js
@@ -0,0 +1,35 @@
+import mysql from "mysql2/promise";
+
+export async function runQuery(query, data) {
+
+try {
+
+const db = await mysql.createConnection({
+
+host: "127.0.0.1",
+
+port: 3306,
+
+database: "fittrek",
+
+user: "root",
+
+password: ""
+
+})
+
+const [result] = await db.execute(query, data);
+
+await db.end();
+
+return result;
+
+} catch (error) {
+
+console.log(error);
+
+return error;
+
+}
+
+}
